9806f21f0eb86db6c61791cf88346e1d4b87d182
mmicek02 <micekmichael@gmail.com>
2020-04-29 18:49:32 -0500

    code written for beerpairing backend

---
+++ src/BeerPairings/beerpairing-router.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const BeerPairingService = require('./beerpairing-service')
+
+const beerPairingRouter = express.Router()
+
+beerPairingRouter
+  .route('/')
+  .get((req, res, next) => {
+    BeerPairingService.getAllArticles(req.app.get('db'))
+      .then(beers => {
+        console.log(beers)
+        res.json(beers.map(BeerPairingService.serializeBeerPairings))
+      })
+      .catch(next)
+  })
+
+beerPairingRouter
+  .route('/:beer_id')
+  .all(checkBeerPairingExists)
+  .get((req, res) => {
+    res.json(BeerPairingService.serializeBeerPairings(res.beer))
+  })
+
+beerPairingRouter.route('/:beer_id/comments/')
+  .all(checkBeerPairingExists)
+  .get((req, res, next) => {
+    BeerPairingService.getCommentsForBeer(
+      req.app.get('db'),
+      req.params.beer_id
+    )
+      .then(comments => {
+        res.json(comments.map(BeerPairingService.serializeBeerPairingsComment))
+      })
+      .catch(next)
+  })
+
+/* async/await syntax for promises */
+async function checkBeerPairingExists(req, res, next) {
+  try {
+    const beer = await BeerPairingService.getById(
+      req.app.get('db'),
+      req.params.beer_id
+    )
+
+    if (!beer)
+      return res.status(404).json({
+        error: `Beer and Donut Pairing doesn't exist`
+      })
+
+    res.beer = beer
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
+
+module.exports = beerPairingRouter
---
+++ src/BeerPairings/beerpairing-service.js
@@ -0,0 +1,43 @@
+const BeerPairingService = {
+    getAllComments(knex) {
+        return knex.select('*').from('beerndonuts_beers')
+    },
+    getById(knex, id) {
+        return knex
+          .from('beerndonuts_beers')
+          .select('*')
+          .where('id', id)
+          .first()
+    },
+    getCommentsForBeer(db, beer_id) {
+        return db
+            .from('beerndonuts_comments AS comm')
+            .select(
+                'comm.id',
+                'comm.text',
+                'comm.date_created',
+            )
+            .where('comm.beer_id', beer_id)
+            .groupBy('comm.id')
+    },
+
+    serializeBeerPairings(beer) {
+        return {
+            id: beer.id,
+            donut_pairing: beer.donut_pairing,
+            tasting_notes: beer.tasting_notes,
+            style: beer.style,
+        }
+    },
+
+    serializeBeerPairingsComment(comment) {
+        return {
+            id: comment.id,
+            beer_id: commment.beer_id,
+            text: comment.text,
+            date_created: new Date(comment.date_created),
+        }
+    },
+}
+
+module.exports = BeerPairingService
