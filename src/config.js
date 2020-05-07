70ec68bc0ef645a57bcca0595fc951be5d74d23a
mmicek02 <micekmichael@gmail.com>
2020-05-07 16:31:17 -0500

    updatae cors

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@lcoalhost/beerndonuts',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@lcoalhost/beerndonuttest',
-    CLIENT_ORIGIN: "https://beerndonut-app.now.sh/",
+    CLIENT_ORIGIN: "https://beerndonut-app.now.sh",
     API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3000",
 }
