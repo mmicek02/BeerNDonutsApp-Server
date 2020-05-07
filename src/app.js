8822fe4c9b00a0f9f990b5a0066ddd8b3113f627
mmicek02 <micekmichael@gmail.com>
2020-05-07 16:36:56 -0500

    update cors

--- src/app.js
+++ src/app.js
@@ -19,18 +19,18 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+);
+
 app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
 
 app.use('/api/comments', commentRouter)
 app.use('/api/beerpairings', beerPairingRouter)
-
-app.use(
-    cors({
-        origin: CLIENT_ORIGIN
-    })
-);
 
 app.use(function errorHandle(error, req, res, next) {
     let response
