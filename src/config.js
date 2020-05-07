8fcbee1e1e77b5af56675fb485bf1da6d3d81f75
mmicek02 <micekmichael@gmail.com>
2020-05-07 15:42:18 -0500

    fixed config.js

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@lcoalhost/beerndonuts',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@lcoalhost/beerndonuttest',
     CLIENT_ORIGIN: "https://beerndonut-app.now.sh/",
     API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3000",
 }
